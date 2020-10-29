95a9e6485fda640b34130df29ef92b59e5e0d7e7
Emiliano Lowe <emilianolowe5@gmail.com>
2020-10-29 17:28:57 +0100

    testing twitter card

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -18,11 +18,11 @@ const query = graphql`
 `
 
 const SEO = ({title, description}) => {
-  const {site} = useStaticQuery(query);
+  const { site } = useStaticQuery(query);
   const {siteDesc, siteTitle, siteUrl, image, twitterUsername} = site.siteMetadata;
 
   return (
-    <Helmet htmlAttributes={{lang:"en"}} title={`${title} | ${siteTitle}`}>
+    <Helmet htmlAttributes={{ lang:"en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
       {/* {Twitter Card} */}
